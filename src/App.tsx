843cc694402eaafb37db52be290df629cff162b6
akmatoff <akmatoff.exe@gmail.com>
2024-06-06 20:12:06 +0600

    add smooth scrolling with lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
 import AboutSection from "./components/AboutSection/AboutSection";
 // import ContactSection from "./components/ContactSection/ContactSection";
 import Cursor from "./components/Cursor/Cursor";
@@ -7,6 +9,17 @@ import WorkSection from "./components/WorkSection/WorkSection";
 import { MouseProvider } from "./contexts/MouseContext";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({ lerp: 0.05 });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="flex flex-col w-screen min-h-screen bg-gradient-to-br from-primary to-secondary">
       <MouseProvider>
